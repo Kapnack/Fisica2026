e9cd2703ebde2c986db17d59a014dc5aecd09c4f
Kapnack <unknowrealperson@gmail.com>
2026-04-09 21:59:15 -0300

    Init Project

---
+++ Assets/Code/PhysicsEntity.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PhysicsEntity : MonoBehaviour
+{
+    [SerializeField] private float coefficientOfRestitution;
+    [SerializeField] private float mass;
+    [SerializeField] private float radious;
+    [SerializeField] private Vector2 position;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
